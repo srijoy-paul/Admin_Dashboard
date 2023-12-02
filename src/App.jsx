804c13f7a070150e5c95d8e2980eba72945db10d
Srijoy Paul <srijoykumar1507@gmail.com>
2023-12-03 00:29:38 +0530

    Implement search on clicking search icon

--- src/App.jsx
+++ src/App.jsx
@@ -35,18 +35,31 @@ function App() {
     })();
   }, []);
 
-  useEffect(() => {
+  // useEffect(() => {
+  //   const filteredData = users.filter((user) => {
+  //     return Object.values(user).some((value) => {
+  //       return String(value).toLowerCase().includes(searchQuery.toLowerCase())
+  //     })
+  //   });
+  //   setFilteredUsers(filteredData);
+  // }, [searchQuery, users]);
+
+  const handleSearchClick = () => {
+    // Trigger search when the search button is clicked
     const filteredData = users.filter((user) => {
       return Object.values(user).some((value) => {
-        return String(value).toLowerCase().includes(searchQuery.toLowerCase())
-      })
+        return String(value).toLowerCase().includes(searchQuery.toLowerCase());
+      });
     });
     setFilteredUsers(filteredData);
-  }, [searchQuery, users]);
+  };
 
   const handlePageChange = ({ selected }) => {
+    console.log(selected);
     setCurrentPage(selected);
   }
+
+  // const handleFirstPageChange=()
 
   const handleSelectedDelete = () => {
     if (selectedUsersId.length === 0) { alert("Not selected"); return; }
@@ -54,10 +67,11 @@ function App() {
     let updateUsers = users.filter(user => !selectedUsersId.includes(user.id));
 
     setUsers(updateUsers);
+    setSelectedUsersId([]);
 
   };
 
-
+  //For Current Page we are in
   const indexOfLastUser = (currentPage + 1) * usersPerPage;
   const indexOfFirstUser = indexOfLastUser - usersPerPage;
   const currentUsers = filteredUsers.slice(indexOfFirstUser, indexOfLastUser);
@@ -71,9 +85,22 @@ function App() {
             <currentUsersContext.Provider value={currentUsersId}>
               <div id='parent-container'>
                 <header>
-                  <Search onSearch={setSearchQuery} />
+                  <div id='search-container'>
+                    <Search onSearch={setSearchQuery} />
+                    <p id='searchButton' onClick={handleSearchClick}>
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                      </svg>
+
+                    </p>
+                  </div>
                   {/* delete button */}
-                  <div id="selectDelete" onClick={handleSelectedDelete}>Select Delete</div>
+                  <div id="selectDelete" onClick={handleSelectedDelete}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                    </svg>
+
+                  </div>
                 </header>
                 <table id='user-container' cellPadding={15} className='table'>
                   <thead>
@@ -87,16 +114,18 @@ function App() {
                 </table>
                 <footer>
 
-                  <div id='selection-info'>0 of {filteredUsers.length} row(s) selected.</div>
+                  <div id='selection-info'>{selectedUsersId.length} of {filteredUsers.length} row(s) selected.</div>
 
                   <div id='pages-info'>
                     <p id='pg-number'>Page {currentPage + 1} of {Math.ceil(filteredUsers.length / usersPerPage)}</p>
 
                     <div id='pagination-container'>
-                      <div className='first-page' onClick={() => handlePageChange({ selected: 0 })}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5" />
-                        </svg>
+                      <div>
+                        <button className='first-page' onClick={() => handlePageChange({ selected: 0 })}>
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5" />
+                          </svg>
+                        </button>
                       </div>
                       {filteredUsers.length > usersPerPage && (
                         <ReactPaginate
@@ -106,16 +135,23 @@ function App() {
                           pageCount={Math.ceil(filteredUsers.length / usersPerPage)}
                           marginPagesDisplayed={2}
                           pageRangeDisplayed={5}
+                          forcePage={currentPage}
                           onPageChange={handlePageChange}
                           containerClassName={'pagination'}
-                          activeClassNameClassName={'active'}
+                          activeClassName={'selected'}
                         />
                       )}
-                      <button className="last-page" onClick={() => {
-                        let lastPage = Math.ceil(filteredUsers.length / usersPerPage);
-                        return handlePageChange({ selected: lastPage - 1 })
-                      }}>
-                      </button>
+                      <div>
+                        <button className="last-page" onClick={() => {
+                          let lastPage = Math.ceil(filteredUsers.length / usersPerPage);
+                          return handlePageChange({ selected: lastPage - 1 })
+                        }}>
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
+                          </svg>
+
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </footer>
--- src/components/UserItem.jsx
+++ src/components/UserItem.jsx
@@ -5,6 +5,9 @@ import { usersContext, selectedUsersIdContext, isSelectAllContext, currentUsersC
 function UserItem({ values }) {
     const { heading, id, name, email, role } = values;
 
+    const [isEditing, setIsEditing] = useState(false);
+    const [editValues, setEditValues] = useState({ name, email, role });
+
 
     let { users, setUsers } = useContext(usersContext);
     let { selectedUsersId, setSelectedUsersId } = useContext(selectedUsersIdContext);
@@ -23,6 +26,24 @@ function UserItem({ values }) {
         setIsSelectAll(!isSelectAll);
         setSelectedUsersId(updateSelectedUsersId);
     };
+
+    function handleUpdate(e, field) {
+        setEditValues((preValue) => ({ ...preValue, [field]: e.target.value }));
+    }
+
+    const handleSaveClick = () => {
+        const updateUsers = users.map(user => (user.id === id) ? { ...user, ...editValues } : user);
+        // console.log("after updating", updateUsers);
+        setUsers(updateUsers);
+        setIsEditing(false);
+    };
+
+    const handleEdit = () => {
+        if (isEditing) handleSaveClick()
+
+        else setIsEditing(!isEditing)
+    }
+
 
     return (
         <>
@@ -54,16 +75,25 @@ function UserItem({ values }) {
                                     document.getElementById(id).classList.remove("userSelected");
                                 }
                             }} /></td>
-                            <td><input type="text" name="" id="" value={name} readOnly /></td>
-                            <td><input type="text" name="" id="" value={email} readOnly /></td>
-                            <td><input type="text" name="" id="" value={role} readOnly /></td>
+
+                            <td><input type="text" name="name" id="" value={editValues.name} readOnly={!isEditing} onChange={(e) => { handleUpdate(e, "name") }} className={id} /></td>
+
+                            <td><input type="text" name="email" id="" value={editValues.email} readOnly={!isEditing} onChange={(e) => { handleUpdate(e, "email") }} className={id} /></td>
+
+                            <td><input type="text" name="role" id="" value={editValues.role} readOnly={!isEditing} onChange={(e) => { handleUpdate(e, "role") }} className={id} /></td>
+
                             <td>
                                 <div id='action-btns'>
-                                    <div id="edit" onClick={() => {
-                                        document.getElementsByTagName("input").readOnly = false;
-                                    }}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                                        </svg>
+                                    <div id="edit" onClick={handleEdit}>
+                                        {isEditing ?
+                                            (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15M9 12l3 3m0 0l3-3m-3 3V2.25" />
+                                            </svg>)
+                                            :
+                                            (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                            </svg>)}
+
                                     </div>
                                     <div id="delete" onClick={() => deleteAction(id)}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
